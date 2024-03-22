46a9346aa822d342851314c8b2c59e7a3791dd4e
ryantoxx <nikitachihai2@gmail.com>
2024-03-22 12:46:53 +0200

    add option command

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,11 @@
 #! /usr/bin/env node
 console.log("Hello World!");
+const yargs = require("yargs");
+
+const usage = "\n Usage: go2web <lang_name> sentence to be translated"; const options = yargs  
+      .usage(usage)  
+      .option("l", {alias:"languages", describe: "List all supported languages.", type: "boolean", demandOption
+: false })                                                                                                    
+      .help(true)  
+      .argv;
+      
